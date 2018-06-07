ee739eefa9f08ec59040d1dcc551ac1f7bd8fb04
Sean Young <sean.young@monax.io>
2018-06-07 14:10:30 +0100

    Use solc from gcloud rather than google drive
    Signed-off-by: Sean Young <sean.young@monax.io>

--- tests/scripts/deps/solc.sh
+++ tests/scripts/deps/solc.sh
@@ -2,7 +2,8 @@
 set -e
 # The version of solc we will fetch and install into ./bin/ for integration testsS
 # Our custom build of solidity fixing linking issue; https://github.com/monax/solidity/tree/truncate-beginning-v0.4.22
-SOLC_URL="https://drive.google.com/uc?export=download&id=1_RHMTxlYHL4zcmZDyXemccR8ENVeAEO2"
+SOLC_URL="https://pkgs.monax.io/apt/solc/0.4.22/linux-amd64/solc"
+
 SOLC_BIN="$1"
 
 wget -O "$SOLC_BIN" "$SOLC_URL"
